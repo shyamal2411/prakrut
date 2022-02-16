725fea1bb1fa55d216fe5d4efebc688484277c28
Shyamal <sgprajapati24@gmail.com>
2022-02-16 23:25:55 +0530

    first part of landing done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
-// import React from "react";
-// import {BrowserRouter as Router, Route,Routes,withRouter,useParams} from "react-router-dom";
-// import "./styles.css";
-// import Navbar from "./components/Navbar/Navbar";
-// import Home from "./pages/Home/Home";
-// import Gallery from "./pages/Gallery/Gallery";
-// import signUp from "./pages/signUp/signUp";
-// import Landing from "./pages/Landing/Landing";
-// import contactUs from "./pages/contactUs/contactUs";
-// // import Login from "./pages/Login/Login";
+import React from "react";
+import {BrowserRouter as Router, Route,Routes,withRouter,useParams} from "react-router-dom";
+import "./styles.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Gallery from "./pages/Gallery/Gallery";
+import signUp from "./pages/signUp/signUp";
+import Landing from "./pages/Landing/Landing";
+import contactUs from "./pages/contactUs/contactUs";
+// import Login from "./pages/Login/Login";
 
-// const App = () => {
-//   return (
-//     // <Router>
-//       <div>
-//         {/* <Navbar /> */}
-//         <Landing/>
-//         {/* <BarLoader loading/> */}
-//          {/* NOTE: ROUTES WORKING AS THEY'RE SHOWING UP ON PAGE BUT NOT SHOWING THE CONTENT SO COMMENTED TEMPORARILY */}
-//           {/* <Routes>
-//            <Route path="/" exact component={Home} />
-//           <Route path="/Gallery" component={Gallery} />
-//           <Route path="/Landing" component={Landing} />
-//           <Route path="/contactUs" component={contactUs} />
-//           <Route path="/signUp" component={signUp} /> 
-//           </Routes> */}
-//       </div>
-//     // </Router>
-//   );
-// }
-
-// export default App;
-
-import React from 'react'
-// import Landing from '../src/pages/Landing/Landing'; 
-import Home from './pages/Home/Home';
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
+    // <Router>
+      <div>
+        <Navbar />
+        <Landing/>
+        {/* <BarLoader loading/> */}
+         {/* NOTE: ROUTES WORKING AS THEY'RE SHOWING UP ON PAGE BUT NOT SHOWING THE CONTENT SO COMMENTED TEMPORARILY */}
+          {/* <Routes>
+           <Route path="/" exact component={Home} />
+          <Route path="/Gallery" component={Gallery} />
+          <Route path="/Landing" component={Landing} />
+          <Route path="/contactUs" component={contactUs} />
+          <Route path="/signUp" component={signUp} /> 
+          </Routes>  */}
+      </div>
+    // </Router>
+  );
 }
 
 export default App;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,34 +1,45 @@
-// import React from "react";
-// import "./Landing.css";
-
-
-// const Landing = () =>{
-// return(
-//   <div>
-//     <div className="landing-container">
-//      <div data-aos="fade-down-right" className="landing-left">
-//         <h1 className="landing-header">This is the header</h1>
-//       </div>
-//     </div>
-//   </div>
-// );
-// };
-// export default Landing;
 
 import React from 'react'
 import './Landing.css';
+import { makeStyles } from '@material-ui/core/styles';
+import flash from './hero.png';
+
+const useStyles = makeStyles((theme) => ({
+  // appBar: {
+  //   backgroundColor: "#fff"
+  // },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1489183988443-b37b7e119ba6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fGhvcml6b250YWwlMjBiYWNrZ3JvdW5kfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
+  }
+}));
 
 function Landing() {
+  // const classes = useStyles(); // this is navbar part,remove this, if asked (BLOG | MOMAN.AI)
  return(
-  <div>
     <div className="landing-container">
      <div data-aos="fade-down-right" className="landing-left">
-        <h1 className="landing-header">This is the header</h1>
-      {/* </div> */}
+        <p className="landing-header">The mango was first grown in 1931 by Junagadh Wazir Sale Bhai in Vanthali. About 75 grafts were then planted in the foothills of Girnar at the Junagadh Laal Dhori Farm. The mango is said to have been known as "KESAR" since 1934 when the Nawab of Junagadh Muhammad Mahabat Khan III said "This is KEASAR" after looking at the orange pulp of the KESAR Mango fruit. The Kesar being hindi for Saffron.</p>
     </div>
-  </div>
+
+    <div className="landing-right">
+    <img src={flash} className='flash-image'/>
+    </div>
   </div>
 );
 };
 
-export default Landing
+export default Landing;
